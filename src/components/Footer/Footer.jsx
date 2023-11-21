f703b6f123918b4c303c6341dc98f787162d1384
lily4178993 <nellytelli@gmail.com>
2023-11-21 13:18:38 +0000

    feat: update footer references links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <footer className="footer">
@@ -7,10 +8,43 @@ const Footer = () => (
       <div className="footer-content">
         <p>&copy; 2023 Dota2 Insight Hub</p>
         <ul className="footer-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="/aboutme">About</a></li>
-          <li><a href="/references">References</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li>
+            <Link
+              to="/"
+              rel="noopener noreferrer"
+              aria-label="Home"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/aboutme"
+              rel="noopener noreferrer"
+              aria-label="About Me"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/references"
+              rel="noopener noreferrer"
+              aria-label="References"
+            >
+              References
+            </Link>
+          </li>
+          <li>
+            <a
+              href="https://github.com/lily4178993/dota2-insight-hub.git"
+              rel="noreferrer"
+              target="_blank"
+              aria-label="GitHub page"
+            >
+              Contact
+            </a>
+          </li>
         </ul>
       </div>
     </div>
