8f59d51323404aaf5031401833ebd73918c1654f
lily4178993 <nellytelli@gmail.com>
2024-03-16 01:18:14 +0000

    feat(state-persistence): Add redux-persist for State Persistence
    Integrated the `redux-persist` package to enable state persistence across sessions. Configured Redux store to persist state data, ensuring user preferences and session data are retained between sessions.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import {
   heroesSlice, itemsSlice, matchesSlice, playersSlice,
 } from './slices';
 
-const store = configureStore({
-  reducer: {
-    heroes: heroesSlice.reducer,
-    items: itemsSlice.reducer,
-    matches: matchesSlice.reducer,
-    players: playersSlice.reducer,
-  },
+const rootReducer = combineReducers({
+  heroes: heroesSlice.reducer,
+  items: itemsSlice.reducer,
+  matches: matchesSlice.reducer,
+  players: playersSlice.reducer,
 });
 
-export default store;
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
