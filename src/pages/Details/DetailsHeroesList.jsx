0dc0ab5bda2f441e4ab2fb0ff178d0191268a9aa
lily4178993 <nellytelli@gmail.com>
2024-03-18 16:32:24 +0000

    feat(component): Init boileplate to refactor details component
    added only component boileplate for heroes list

---
+++ src/pages/Details/DetailsHeroesList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DetailsHeroesList = () => {
+  // Will be implemented in a future version
+  // For now, it will just return a placeholder     
+  // This component will be used to display the list of heroes
+  return (
+    <div>DetailsHeroesList</div>
+  )
+}
+
+export default DetailsHeroesList
