6007b47afe6337d355593888959765fd3c4f12cd
lily4178993 <nellytelli@gmail.com>
2024-03-23 13:13:31 +0000

    refactor(Details): Integrate DataLists into Details Component
    Refactored Details component to incorporate DataLists components, improving data presentation and component maintainability.

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,89 +1,29 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
-import { Card } from '../../components';
-import matchesImage from '../../assets/trophy.png';
+import {
+  HeroesList, ItemsList, ProPlayersList, PromatchesList,
+} from './DataLists';
+import {
+  selectHeroesState, selectItemsState, selectMatchesState, selectPlayersState,
+} from '../../redux/slices';
 import './details.css';
-import generateHeroPosterlink from '../../utils/generateHeroPosterlink';
 
 function Details() {
   const { detailsListName } = useParams();
-  const detailsData = useSelector((state) => {
-    switch (detailsListName) {
-      case 'heroes':
-        return state.heroes.heroes;
-      case 'items':
-        return state.items.items;
-      case 'proMatches':
-        return state.matches.matches;
-      case 'proPlayers':
-        return state.players.players;
-      default:
-        return null;
-    }
-  });
 
-  // Ensure that detailsData is an array
-  const detailsDataArray = Array.isArray(detailsData) ? detailsData : [];
-
-  const mapDataProperties = (data) => {
-    switch (detailsListName) {
-      case 'heroes':
-        return {
-          cardImage: generateHeroPosterlink(`https://api.opendota.com${data?.img}`),
-          cardTitle: data?.localized_name,
-          cardCount: data?.base_str,
-          dataKey: data?.id,
-        };
-      case 'items':
-        return {
-          cardImage: `https://api.opendota.com${data?.img}`,
-          cardTitle: data?.dname,
-          cardCount: data?.cost || 'N/A',
-          dataKey: data?.key,
-        };
-      case 'proMatches':
-        return {
-          cardImage: matchesImage,
-          cardTitle: data?.dire_name || 'N/A',
-          cardCount: data?.dire_score,
-          dataKey: data?.match_id,
-        };
-      case 'proPlayers':
-        return {
-          cardImage: data?.avatarmedium,
-          cardTitle: data?.personaname,
-          cardCount: data?.account_id,
-          dataKey: data?.account_id,
-        };
-      default:
-        return null;
-    }
-  };
+  const { heroes } = useSelector(selectHeroesState);
+  const { items } = useSelector(selectItemsState);
+  const { matches } = useSelector(selectMatchesState);
+  const { players } = useSelector(selectPlayersState);
 
   return (
     <section>
-      <div className="detailsList-container">
-        {detailsDataArray.length > 0 ? (
-          detailsDataArray.map((data) => {
-            const mappedData = mapDataProperties(data);
-            if (!mappedData) {
-              return null;
-            }
-
-            return (
-              <Link to={`/details/${detailsListName}/${mappedData.dataKey}`} key={mappedData.dataKey}>
-                <Card
-                  cardImage={mappedData.cardImage}
-                  cardTitle={mappedData.cardTitle}
-                  cardCount={mappedData.cardCount}
-                />
-              </Link>
-            );
-          })
-        ) : (
-          'Data not found'
-        )}
+      <div>
+        {detailsListName === 'heroes' && (<HeroesList heroesData={heroes} />)}
+        {detailsListName === 'items' && (<ItemsList itemsData={items} />)}
+        {detailsListName === 'proMatches' && (<PromatchesList proMatchesData={matches} />)}
+        {detailsListName === 'proPlayers' && (<ProPlayersList proPlayersData={players} />)}
       </div>
     </section>
   );
