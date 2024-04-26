0b03109d92d9cc230947230f8048ed49121abbdc
lily4178993 <nellytelli@gmail.com>
2024-04-26 17:28:51 +0000

    feat(Swiper): Register Swiper custom elements and import necessary CSS files
    Added the registration of Swiper custom elements and imported the required CSS files for proper styling and functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 /* eslint-disable object-curly-newline */
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { register } from 'swiper/element/bundle';
 import { AboutMe, Details, DetailsItem, Home, References } from './pages';
 import AppLayout from './pages/layouts/AppLayout';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// register Swiper custom elements
+register();
 
 const routesList = [
   { path: '/', element: <Home /> },
