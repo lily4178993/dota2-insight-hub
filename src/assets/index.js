b73175cf3001f8b105d129b931b225d8901fb881
lily4178993 <nellytelli@gmail.com>
2024-04-26 17:23:59 +0000

    refactor(assets): Remove unused banner images

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,4 @@ export { default as IconGrid } from './grid-icon.svg';
 export { default as IconSearch } from './flashlight.svg';
 export { default as IconFilter } from './funnel.svg';
 export { default as IconClose } from './sword-spade.svg';
-export { default as BannerHeroes1 } from './banner-heroes-1.jpeg';
-export { default as BannerHeroes2 } from './banner-heroes-2.jpeg';
-export { default as BannerHeroes3 } from './banner-heroes-3.jpeg';
 export { default as BannerHeroes4 } from './banner-heroes-4.jpeg';
