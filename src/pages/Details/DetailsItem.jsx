e0189d8d3d90e0545fd671581da88b2342ebd6f7
lily4178993 <nellytelli@gmail.com>
2024-04-26 17:59:29 +0000

    feat(DetailsItem): Refactor DetailsItem component to use useSelector
    - Refactored DetailsItem component to use useSelector
    - Added useParams hooks for improved code readability and performance

--- src/pages/Details/DetailsItem.jsx
+++ src/pages/Details/DetailsItem.jsx
@@ -1,38 +1,34 @@
 import React from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import {
-  HeroInfo,
   ItemPresentation,
   MatchInfo,
   ProPlayerProfile,
 } from '../../components';
 import './detailsItem.css';
+import {
+  selectHeroesState, selectItemsState, selectMatchesState, selectPlayersState,
+} from '../../redux/slices';
+import IndividualHeroPage from '../Heroes/IndividualHeroPage';
 
 function DetailsItem() {
   const { detailsItemID } = useParams();
-  const location = useLocation();
-  const detailsListName = location.pathname.split('/')[2];
-  const detailsData = useSelector((state) => {
-    switch (detailsListName) {
-      case 'heroes':
-        return state.heroes.heroes;
-      case 'items':
-        return state.items.items;
-      case 'proMatches':
-        return state.matches.matches;
-      case 'proPlayers':
-        return state.players.players;
-      default:
-        return [];
-    }
-  });
+  const { detailsListName } = useParams();
+
+  const { heroes } = useSelector(selectHeroesState);
+  const { items } = useSelector(selectItemsState);
+  const { matches } = useSelector(selectMatchesState);
+  const { players } = useSelector(selectPlayersState);
+
+  let index = '' || null;
 
   const renderComponent = () => {
     switch (detailsListName) {
       case 'heroes':
-        if (detailsData && detailsData.length > 0 && detailsData[detailsItemID]) {
-          return <HeroInfo data={detailsData[detailsItemID - 1]} />;
+        if (heroes && heroes.length > 0) {
+          index = heroes.find((hero) => hero.localized_name === detailsItemID);
+          return <IndividualHeroPage hero={index} />;
         }
         return (
           <div className="error-fetch">
@@ -42,10 +38,10 @@ function DetailsItem() {
           </div>
         );
       case 'items':
-        if (detailsData && detailsData.length > 0) {
-          const selectedItem = detailsData.find((item) => item.key === detailsItemID);
-          if (selectedItem) {
-            return <ItemPresentation data={selectedItem} />;
+        if (items && items.length > 0) {
+          index = items.findIndex((item) => item.key === detailsItemID);
+          if (index) {
+            return <ItemPresentation data={index} />;
           }
         }
         return (
@@ -56,12 +52,12 @@ function DetailsItem() {
           </div>
         );
       case 'proMatches':
-        if (detailsData && detailsData.length > 0) {
-          const index = detailsData.findIndex(
+        if (matches && matches.length > 0) {
+          index = matches.findIndex(
             (object) => object.match_id === Number(detailsItemID),
           );
           if (index !== -1) {
-            return <MatchInfo data={detailsData[index]} />;
+            return <MatchInfo data={matches[index]} />;
           }
         }
         return (
@@ -72,12 +68,12 @@ function DetailsItem() {
           </div>
         );
       case 'proPlayers':
-        if (detailsData && detailsData.length > 0) {
-          const index2 = detailsData.findIndex(
+        if (players && players.length > 0) {
+          index = players.findIndex(
             (object) => object.account_id === Number(detailsItemID),
           );
-          if (index2 !== -1) {
-            return <ProPlayerProfile data={detailsData[index2]} />;
+          if (index !== -1) {
+            return <ProPlayerProfile data={players[index]} />;
           }
         }
         return (
