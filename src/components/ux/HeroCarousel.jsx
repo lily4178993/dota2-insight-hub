60b6850d1f011256bc56fa9257ad9ee64865efbe
lily4178993 <nellytelli@gmail.com>
2024-05-10 03:27:26 +0000

    chore(HeroCarousel): Refactor HeroCarousel component
    - Remove unused imports and code
    - Update Swiper speed and autoplay settings
    - Refactor slide data structure and rendering logic

--- src/components/ux/HeroCarousel.jsx
+++ src/components/ux/HeroCarousel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Parallax, Pagination, Navigation } from 'swiper/modules';
+import { Parallax } from 'swiper/modules';
 import { BannerHeroes4 } from '../../assets';
 
 // Define a structure to hold the data for each slide
@@ -10,50 +10,43 @@ const slidesData = [
     title: 'Embark on Your Epic Quest',
     description:
       'Begin your journey through the vast landscapes of Dota 2. Here, heroes await to claim their glory. Discover your champion and forge your path to victory.',
-    image: BannerHeroes4,
   },
   {
     id: 2,
     title: 'Enter the Battlefield',
     description:
       'Step into a realm where strategy and skill collide. With heroes from every corner of the cosmos, find the one who resonates with your battle cry.',
-    image: BannerHeroes4,
   },
   {
     id: 3,
     title: 'Legends Await Your Call',
     description:
       'Every hero has a tale, and every tale needs a hero. Dive into the rich lore of Dota 2 and emerge as the legend you\'re destined to become.',
-    image: BannerHeroes4,
   },
   {
     id: 4,
     title: 'Forge Your Legacy',
     description:
-      'The ancients call for a hero; will you answer? Explore the myriad of champions and unleash your potential on the battlegrounds of Dota 2.',
-    image: BannerHeroes4,
+      'The ancients call for a hero, will you answer? Explore the myriad of champions and unleash your potential on the battlegrounds of Dota 2.',
   },
 ];
 
 function HeroCarousel() {
   return (
     <Swiper
-      style={{
-        '--swiper-navigation-color': '#fff',
-        '--swiper-pagination-color': '#fff',
-      }}
-      speed={600}
+      speed={3000}
       parallax
-      pagination={{
-        clickable: true,
+      modules={[Parallax]}
+      simulateTouch={false}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
       }}
-      navigation
-      modules={[Parallax, Pagination, Navigation]}
-      className="mySwiper size-full relative"
+      className="size-full relative"
     >
       <div
         slot="container-start"
-        className="parallax-bg"
+        className="parallax-bg grayscale-50 opacity-50"
         style={{
           'background-image': `url(${BannerHeroes4})`,
         }}
@@ -61,19 +54,9 @@ function HeroCarousel() {
       />
       {slidesData.map((slide) => (
         <SwiperSlide key={slide.id}>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 1
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
+          <div className="size-full flex flex-col 2xl:flex-row 2xl:items-center justify-center font-light text-center md:text-left text-balance bg-gradient-to-t from-black from-10% py-10 md:px-16">
+            <h1 className="2xl:w-[850px] text-4xl md:text-8xl" data-swiper-parallax="-300">{slide.title}</h1>
+            <p className="md:w-[850px] md:text-4xl mt-4 2xl:mt-0" data-swiper-parallax="-100">{slide.description}</p>
           </div>
         </SwiperSlide>
       ))}
