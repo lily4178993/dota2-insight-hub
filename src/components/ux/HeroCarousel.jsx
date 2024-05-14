27b11234d737bc2d57d67749c039c112e41f0b88
lily4178993 <nellytelli@gmail.com>
2024-05-14 11:24:41 +0000

    chore(HeroCarousel): Refactor HeroCarousel component
    - made this carousel reusable
    - passed the slidesData and backgroundImage as props to the HeroCarousel component.

--- src/components/ux/HeroCarousel.jsx
+++ src/components/ux/HeroCarousel.jsx
@@ -1,37 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Parallax } from 'swiper/modules';
-import { BannerHeroes4 } from '../../assets';
 
-// Define a structure to hold the data for each slide
-const slidesData = [
-  {
-    id: 1,
-    title: 'Embark on Your Epic Quest',
-    description:
-      'Begin your journey through the vast landscapes of Dota 2. Here, heroes await to claim their glory. Discover your champion and forge your path to victory.',
-  },
-  {
-    id: 2,
-    title: 'Enter the Battlefield',
-    description:
-      'Step into a realm where strategy and skill collide. With heroes from every corner of the cosmos, find the one who resonates with your battle cry.',
-  },
-  {
-    id: 3,
-    title: 'Legends Await Your Call',
-    description:
-      'Every hero has a tale, and every tale needs a hero. Dive into the rich lore of Dota 2 and emerge as the legend you\'re destined to become.',
-  },
-  {
-    id: 4,
-    title: 'Forge Your Legacy',
-    description:
-      'The ancients call for a hero, will you answer? Explore the myriad of champions and unleash your potential on the battlegrounds of Dota 2.',
-  },
-];
-
-function HeroCarousel() {
+function HeroCarousel({ slidesData, backgroundImage }) {
   return (
     <Swiper
       speed={3000}
@@ -48,7 +20,7 @@ function HeroCarousel() {
         slot="container-start"
         className="parallax-bg grayscale-50 opacity-50"
         style={{
-          'background-image': `url(${BannerHeroes4})`,
+          'background-image': `url(${backgroundImage})`,
         }}
         data-swiper-parallax="-23%"
       />
@@ -63,5 +35,14 @@ function HeroCarousel() {
     </Swiper>
   );
 }
+
+HeroCarousel.propTypes = {
+  slidesData: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  backgroundImage: PropTypes.string,
+};
+
+HeroCarousel.defaultProps = {
+  backgroundImage: '',
+};
 
 export default HeroCarousel;
