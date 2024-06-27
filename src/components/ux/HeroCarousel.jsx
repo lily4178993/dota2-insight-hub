31f1db5e11094aa538a2a5be4fc23647a410d86d
lily4178993 <nellytelli@gmail.com>
2024-06-27 15:08:46 +0000

    feat: Refactor HeroCarousel component
    Refactor the HeroCarousel component to improve reusability and optimize performance. Pass the slidesData and backgroundImage as props to the HeroCarousel component. Remove unused imports and code. Update Swiper speed and autoplay settings. Refactor the slide data structure and rendering logic.

--- src/components/ux/HeroCarousel.jsx
+++ src/components/ux/HeroCarousel.jsx
@@ -1,34 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Parallax } from 'swiper/modules';
 
-function HeroCarousel({ slidesData, backgroundImage }) {
+/**
+ * Renders a hero carousel component.
+ *
+ * @param {Object} props - The component props.
+ * @param {Array} props.slidesData - The data for the carousel slides.
+ * @param {string} props.carouselImage - The URL of the carousel background image.
+ * @param {Array|string} props.sliderModule - The modules to enable for the carousel.
+ * @param {number} props.sliderSpeed - The speed of the carousel slide transition.
+ * @param {string} props.sliderEffect - The effect to apply to the carousel slides.
+ * @returns {JSX.Element} The rendered hero carousel component.
+ */
+function HeroCarousel({
+  slidesData, carouselImage, sliderModule, sliderSpeed, sliderEffect,
+}) {
+  const isSliderModuleArray = Array.isArray(sliderModule);
+
   return (
     <Swiper
-      speed={3000}
-      parallax
-      modules={[Parallax]}
+      effect={sliderEffect || 'fade'}
+      fadeEffect={{ crossFade: true }}
+      modules={isSliderModuleArray ? sliderModule : undefined}
+      speed={sliderSpeed || 1000}
+      loop
       simulateTouch={false}
       autoplay={{
-        delay: 5000,
         disableOnInteraction: false,
       }}
-      className="size-full relative"
+      className="relative size-full"
     >
       <div
         slot="container-start"
-        className="parallax-bg grayscale-50 opacity-50"
+        className={`opacity-50 parallax-bg grayscale-50 ${isSliderModuleArray && sliderModule.includes('Parallax') ? 'data-swiper-parallax="-23%"' : ''}`}
         style={{
-          'background-image': `url(${backgroundImage})`,
+          backgroundImage: `url(${carouselImage})`,
         }}
-        data-swiper-parallax="-23%"
       />
       {slidesData.map((slide) => (
-        <SwiperSlide key={slide.id}>
+        <SwiperSlide
+          key={slide.id}
+          style={{
+            background: `url(${slide?.image}) no-repeat top/cover, #0000008c`,
+            backgroundBlendMode: 'multiply',
+            isolation: 'isolate',
+          }}
+        >
           <div className="size-full flex flex-col 2xl:flex-row 2xl:items-center justify-center font-light text-center md:text-left text-balance bg-gradient-to-t from-black from-10% py-10 md:px-16">
-            <h1 className="2xl:w-[850px] text-4xl md:text-8xl" data-swiper-parallax="-300">{slide.title}</h1>
-            <p className="md:w-[850px] md:text-4xl mt-4 2xl:mt-0" data-swiper-parallax="-100">{slide.description}</p>
+            <h1 className={`2xl:w-[850px] text-4xl md:text-7xl ${isSliderModuleArray && sliderModule.includes('Parallax') ? 'data-swiper-parallax="-300"' : ''}`}>{slide.title}</h1>
+            <p className={`md:w-[850px] md:text-4xl mt-4 2xl:mt-0 ${isSliderModuleArray && sliderModule.includes('Parallax') ? 'data-swiper-parallax="-200"' : ''}`}>{slide.description}</p>
           </div>
         </SwiperSlide>
       ))}
@@ -38,11 +59,17 @@ function HeroCarousel({ slidesData, backgroundImage }) {
 
 HeroCarousel.propTypes = {
   slidesData: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-  backgroundImage: PropTypes.string,
+  carouselImage: PropTypes.string,
+  sliderModule: PropTypes.arrayOf(PropTypes.func),
+  sliderEffect: PropTypes.string,
+  sliderSpeed: PropTypes.number,
 };
 
 HeroCarousel.defaultProps = {
-  backgroundImage: '',
+  carouselImage: '',
+  sliderModule: [],
+  sliderEffect: 'fade',
+  sliderSpeed: 1000,
 };
 
 export default HeroCarousel;
