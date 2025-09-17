bc45c42afde3f5fac45d9a97c950d38b4f7ed8a0
lily4178993 <nellytelli@gmail.com>
2025-09-17 18:05:14 +0000

    feat: Remove categorizeItem utility function and update exports

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,5 @@ export { default as attributeDictionary } from './attributeDictionary';
 export { default as generateHeroAbilityIconlink } from './generateHeroAbilityIconlink';
 export { default as generateHeroPosterlink } from './generateHeroPosterlink';
 export { default as generateItemIconlink } from './generateItemIconlink';
-export { default as categorizeItem } from './categorizeItem';
 export { default as getExpertCategorizedItems } from './getExpertCategorizedItems';
 export { default as getBeginnerCategorizedItems } from './getBeginnerCategorizedItems';
