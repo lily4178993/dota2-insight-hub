30e076ebf8505ce1cc196f204133eeb104354e58
lily4178993 <nellytelli@gmail.com>
2026-09-18 21:20:17 +0000

    chore: Add new keyframes & screens to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,35 @@ module.exports = {
           '0%': { transform: 'perspective(1000px) rotateY(360deg)' },
           '100%': { transform: 'perspective(1000px) rotateY(0deg)' },
         },
+        scrollLeft: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': {
+            transform: 'translateX(calc(-50% - var(--gap)))',
+          },
+        },
+        scrollRight: {
+          '0%': { transform: 'translateX(calc(-50% - var(--gap)))' },
+          '100%': {
+            transform: 'translateX(0)',
+          },
+        },
       },
       animation: {
         rotate: 'rotate 30s linear infinite',
+        'scroll-left': 'scrollLeft 30s linear infinite',
+        'scroll-right': 'scrollRight 30s linear infinite',
       },
       fontFamily: {
         ica: ['ICA Rubrik', 'sans-serif'],
       },
     },
     screens: {
-      xs: '320px',
-      sm: '360px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      xs: '320px', // extra small
+      sm: '360px', // small medium
+      smx: '540px', // small medium extra
+      md: '768px', // medium
+      lg: '1024px', // large
+      xl: '1280px', // extra large
       '2xl': '1536px',
     },
   },
